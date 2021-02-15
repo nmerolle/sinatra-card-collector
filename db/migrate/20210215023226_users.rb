05d7cc9fb7bec5b63a867ba4c050c95dec9a2ca5
nmerolle <nmerolle11@hotmail.com>
2021-02-15 00:58:52 -0500

    ran rake db:migration

--- db/migrate/20210215023226_users.rb
+++ db/migrate/20210215023226_users.rb
@@ -3,7 +3,7 @@ class Users < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :username
       t.string :email
-      t.string password_digest
+      t.string :password_digest
     end
   end
 end
